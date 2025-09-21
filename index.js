8aeced5d3756987a43d3adf5d5458000477677b2
ALP300 <aitorpalomino1@gmail.com>
2025-09-20 20:58:13 -0500

    a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Este es el primer cambio")
+
+let a=23231283812312.7123123
+let b= "HAOLASDAKDSADSA"
+let c= 23
+let a1= parseInt(prompt("Hola por favor, ingresa un valor1: "))
+let a2= parseInt(prompt("Hola por favor, ingresa un valor2: "))
+let suma= a1+a2
+let resta= a1-a2
+let mult= a1*a2
+let div= a1/a2
+let modulo= a1%a2
+console.log(a1)
+console.log(a2)
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(suma)
+console.log(mult)
+console.log(div)
+console.log(modulo)
