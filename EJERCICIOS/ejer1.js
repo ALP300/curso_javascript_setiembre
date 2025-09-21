c37b29e1949b2e1170cbcb9c60441f24dd66b66d
ALP300 <aitorpalomino1@gmail.com>
2025-09-20 20:57:51 -0500

    a

---
+++ EJERCICIOS/ejer1.js
@@ -0,0 +1,24 @@
+/*
+1. Determina el mayor de tres números: 
+Declara tres variables a, b y c con valores numéricos y 
+utiliza condicionales  para determinar cuál de ellos es el mayor.
+*/
+let a=23231283812312.7123123
+let b= "HAOLASDAKDSADSA"
+let c= 23
+let a1= parseInt(prompt("Hola por favor, ingresa un valor1: "))
+let a2= parseInt(prompt("Hola por favor, ingresa un valor2: "))
+let suma= a1+a2
+let resta= a1-a2
+let mult= a1*a2
+let div= a1/a2
+let modulo= a1%a2
+console.log(a1)
+console.log(a2)
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(suma)
+console.log(mult)
+console.log(div)
+console.log(modulo)
