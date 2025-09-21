bc6a09cc821b8617473cb2b1c0bbbe25c5168ae9
ALP300 <aitorpalomino1@gmail.com>
2025-09-20 21:12:38 -0500

    a

--- EJERCICIOS/ejer1.js
+++ EJERCICIOS/ejer1.js
@@ -3,3 +3,15 @@
 Declara tres variables a, b y c con valores numéricos y 
 utiliza condicionales  para determinar cuál de ellos es el mayor.
 */
+let a= 10
+let b=20
+let c=30
+
+if (a>b && a>c){
+    console.log("El mayor es: ", a)
+} else if (b>a && b>c){
+    console.log("El mayor es: ", b)
+} else{
+    console.log("El mayor es: ", c)
+}
+
