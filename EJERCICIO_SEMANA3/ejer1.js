9cf329c494ea6fb51e51aac2542741339a0c941d
ALP300 <aitorpalomino1@gmail.com>
2025-09-27 20:46:52 -0500

    a

---
+++ EJERCICIO_SEMANA3/ejer1.js
@@ -0,0 +1,51 @@
+/*
+Búsqueda avanzada de elementos:
+Crea un array de objetos con productos (nombre, categoría, precio). Solicita una
+categoría y muestra todos los productos de esa categoría ordenados por precio.
+*/
+function busquedadAvanzada() {
+  const productos = [
+    {
+      nombre: "Iphone 17",
+      categoría: "Electrónicos",
+      precio: 5000,
+    },
+    {
+      nombre: "Celular",
+      categoría: "Electrónicos",
+      precio: 1200,
+    },
+    {
+      nombre: "Polo",
+      categoría: "Ropa",
+      precio: 90,
+    },
+    {
+      nombre: "Zapatillas",
+      categoría: "Ropa",
+      precio: 200,
+    },
+  ];
+
+  function buscarCategoria(categoría) {
+    const productosFiltrados = productos.filter(
+      producto => producto.categoría.toLowerCase() === categoría.toLowerCase()).sort((a,b)=> a.precio-b.precio);
+    
+    if(productosFiltrados.length==0){
+        console.log(`No se encontraron productos en la categoría ${categoría}`)
+        return []
+    }
+    console.log(
+      `Productos en la categoría ${categoría} : `
+    );
+
+    productosFiltrados.forEach((producto, index) => {
+        console.log(`${index + 1}. ${producto.nombre} - $${producto.precio}`);
+    });
+
+    return productosFiltrados;
+  }
+
+  buscarCategoria("Ropa")
+}
+busquedadAvanzada();
